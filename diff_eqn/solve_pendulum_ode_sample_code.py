bacda53e394333f162a78303822e42b6edc129d4
Daniel Greinke <daniel@mac.lan>
2025-02-26 14:20:57 -0800

    add credit to 3b1b

--- diff_eqn/solve_pendulum_ode_sample_code.py
+++ diff_eqn/solve_pendulum_ode_sample_code.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+# Note: credit to 3blue1brown -> https://github.com/3b1b/videos/blob/1385f6d12cabf0c7cd61bdd9b78987e77b9edfbf/_2019/diffyq/solve_pendulum_ode_sample_code.py
 # Physical constants
 g = 9.8
 L = 2
