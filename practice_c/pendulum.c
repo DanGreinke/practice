780cf6a7bdeb1c4330463b2516c9716eaee7c666
Daniel Greinke <daniel@mac.lan>
2025-03-16 11:48:40 -0700

    Remove my definition of Pi, and use M_PI from math.h

--- practice_c/pendulum.c
+++ practice_c/pendulum.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14159265
 #define G 9.8
 #define L 2.0
 #define mu 0.1
 
 #define THETA_DOT_0 0.0
-#define THETA_0 PI / 3.0
+#define THETA_0 M_PI / 3.0
 
 double theta_dot_dot(double theta, double theta_dot){
     return -mu * theta_dot - (G / L) * sin(theta);
@@ -20,7 +19,7 @@ int main(void){
     double theta = THETA_0;
     double theta_dot = THETA_DOT_0;
     double delta_t = 0.01;
-    
+
     for(t=0.0; t<=2.0; t+=delta_t){
         theta_double_dot = theta_dot_dot(theta, theta_dot);
         theta += theta_dot * delta_t;
