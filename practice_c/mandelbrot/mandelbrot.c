a394ce0362ba3d53d47d8546b716cd67221eefe2
Daniel Greinke <daniel@mac.lan>
2025-04-14 23:08:22 -0700

    Implement memory allocation and pointers

--- practice_c/mandelbrot/mandelbrot.c
+++ practice_c/mandelbrot/mandelbrot.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 /* ------------------------------------------------------------------------------------
 Calculate the Mandelbrot set, generate and save an image of it
@@ -32,8 +33,8 @@ Usage Note: PPM format is chosen for ease of use. I recommend exporting to a PNG
 then deleting the PPM file.
 ------------------------------------------------------------------------------------ */
 
-#define HEIGHT 1440
-#define WIDTH 2560
+#define HEIGHT 2520
+#define WIDTH 4480
 double y_min = -0.390625; //-1.125;
 double y_max = -0.3375; //1.125;
 double x_min = -0.75; //-2.0;
@@ -44,18 +45,18 @@ int max_iterations = 1000;
 // new y -> 0.783
 // 800 by 470
 // 860 by 504
-typedef struct {
-    int arr[HEIGHT][WIDTH]; // 2D array to store iterations
-} iteration_array;
+// typedef struct {
+//     int arr[HEIGHT][WIDTH]; // 2D array to store iterations
+// } iteration_array;
 
-int mandelbrot(double x_in, double y_in){
+int mandelbrot(double x_in, double y_in) {
     double x_0 = x_in;
     double y_0 = y_in;
     double x = 0;
     double y = 0;
     int iterations = 0;
     double x_tmp;
-    while (x*x + y*y < 4.0f && iterations < max_iterations){
+    while (x*x + y*y < 4.0f && iterations < max_iterations) {
         x_tmp = x*x - y*y + x_0;
         y = 2.0f*x*y + y_0;
         x = x_tmp;
@@ -64,27 +65,35 @@ int mandelbrot(double x_in, double y_in){
     return iterations;
 }
 
-iteration_array generate_iteration_array(double x_min, double x_max, double y_min, double y_max){
-    iteration_array out_array;
+int* generate_iteration_array(double x_min, double x_max, double y_min, double y_max) {
     int height = HEIGHT;
     int width = WIDTH;
     double x_step = (x_max - x_min) / width;
     double y_step = (y_max - y_min) / height;
     double x_in, y_in;
+    // Allocate memory on the heap
+    int *out_array_data = malloc(height * width * sizeof(int));
+
+    // Check if memory allocation was successful
+    if (out_array_data == NULL) {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
     // Loop over row of pixels
-    for (int i = 0; i < height; i++){
+    for (int i = 0; i < height; i++) {
         // Loop over pixel in row (column)
-        for (int j = 0; j < width; j++){      
+        for (int j = 0; j < width; j++) {      
             x_in = x_min + j * x_step;
             y_in = y_min + i * y_step;
-            out_array.arr[i][j] = mandelbrot(x_in, y_in);
+            out_array_data[i*width + j] = mandelbrot(x_in, y_in);
         }
     }
-    return out_array;
+    return out_array_data;
 }
 
-int color_map(int iterations){
-    if (iterations == max_iterations){
+int color_map(int iterations) {
+    if (iterations == max_iterations) {
         return 0; // Black
     }
     else{
@@ -92,8 +101,10 @@ int color_map(int iterations){
     }
 }
 
-int main(void){
-    iteration_array array = generate_iteration_array(x_min, x_max, y_min, y_max);
+int main(void) {
+    // Receive pointer from generate_iteration_array function
+    int *iteration_data = generate_iteration_array(x_min, x_max, y_min, y_max);
+
     FILE *img;
     int i, j;
     int height = HEIGHT;
@@ -101,20 +112,26 @@ int main(void){
 
     // Initialize Image and Write Header
     img = fopen("mandelbrot.ppm", "w");
-    // Writing Magic Number to the File 
+    if (img == NULL) {
+        perror("Error opening file");
+        free(iteration_data);
+        return 1;
+    }
+    // Writing Magic Number, dimensions, & max color value
     fprintf(img, "P2\n");  
-    // Writing WIDTH and HEIGHT 
     fprintf(img, "%d %d\n", width, height);  
-    // Writing the maximum gray value 
     fprintf(img, "255\n");
+
     // Loop over row in image
-    for (i = 0; i < height; i++){
+    for (i = 0; i < height; i++) {
         // Loop over pixel in row (column)
-        for (j = 0; j < width; j++){
-            fprintf(img, "%d ", color_map(array.arr[i][j]));
+        for (j = 0; j < width; j++) {
+            fprintf(img, "%d ", color_map(iteration_data[i*width + j]));
         }
         fprintf(img, "\n");
     }
     fclose(img);
+    // Free the allocated memory
+    free(iteration_data);
     return 0;
 }
